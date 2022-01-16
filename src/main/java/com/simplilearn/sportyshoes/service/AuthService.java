9c1b9534a20b96e85fe650ad130ef416a7aefe2a
Eric Miranda <Eric.Miranda@trivium-esolutions.com>
2022-01-16 16:44:13 +0530

    project bringup

---
+++ src/main/java/com/simplilearn/sportyshoes/service/AuthService.java
@@ -0,0 +1,21 @@
+package com.simplilearn.sportyshoes.service;
+
+import com.simplilearn.sportyshoes.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.security.auth.login.LoginException;
+
+@Service
+public class AuthService {
+    private final UserService userService;
+
+    public AuthService(UserService userService) {
+        this.userService = userService;
+    }
+
+    public void authenticateUser(String username, String pass) throws LoginException {
+        User user = userService.getUserByUsername(username);
+        if (!(user.getUsername().equals(username) && user.getPassword().equals(pass))) throw new LoginException();
+    }
+}
